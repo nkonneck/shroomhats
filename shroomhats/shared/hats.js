1f9a663c0900cca2e6d030785c4e32b4c760549c
Neil Konnecke <neil.koennecke@gmail.com>
2023-03-26 09:48:27 -0700

    initial setup

---
+++ shroomhats/shared/hats.js
@@ -0,0 +1,11 @@
+export const HATS = [
+    {
+        id: 0,
+        name: "Yoda Hat",
+        image: require("../assets/images/yodahat.png"),
+        price: "34.99",
+        description: "It’s your turn to save the galaxy – while wearing a Shroom Hat of course! An excellent choice for those who are mushroom lovers and force users alike. May the shrooms be with you!",
+        details: "Stretch band",
+        material: "63% polyester, 34% cotton, 3% spandex twill. Structured, mid-profile fit. Six panels with eyelets. Sourced from Vietnam or Bangladesh"
+    }
+];
