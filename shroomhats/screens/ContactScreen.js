52be4f7157ebdbe910621e4623187fc66d2ba2e7
Neil Konnecke <neil.koennecke@gmail.com>
2023-04-09 14:36:28 -0700

    beginning building out contact us page

--- shroomhats/screens/ContactScreen.js
+++ shroomhats/screens/ContactScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Linking } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Linking, ScrollView } from "react-native";
 import { AntDesign, FontAwesome } from '@expo/vector-icons';
- 
+import { Card } from "react-native-elements";
+
 const ContactScreen = () => {
     const handleFacebookPress = () => {
         Linking.openURL('https://www.facebook.com/shroomhats');
@@ -16,20 +17,26 @@ const ContactScreen = () => {
     };
 
     return (
-        <View style={styles.contact}>
-            <Text style={styles.text}>© 2023 Shroom Hats. All rights reserved.</Text>
-            <View style={styles.icons}>
-                <TouchableOpacity onPress={handleFacebookPress}>
-                    <AntDesign name="facebook-square" size={24} color="white" style={styles.icon} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleInstagramPress}>
-                    <AntDesign name="instagram" size={24} color="white" style={styles.icon} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleRedditPress}>
-                    <FontAwesome name="reddit-square" size={24} color="white" style={styles.icon} />
-                </TouchableOpacity>
+        <ScrollView>
+            <Card wrapperStyle={{ margin: 20 }}>
+                <Card.Title h2>Contact Us</Card.Title>
+                <Card.Divider style={{ borderWidth: 2, borderColor: 'black'}} />
+            </Card>
+            <View style={styles.contact}>
+                <View style={styles.icons}>
+                    <TouchableOpacity onPress={handleFacebookPress}>
+                        <AntDesign name="facebook-square" size={24} color="white" style={styles.icon} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={handleInstagramPress}>
+                        <AntDesign name="instagram" size={24} color="white" style={styles.icon} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={handleRedditPress}>
+                        <FontAwesome name="reddit-square" size={24} color="white" style={styles.icon} />
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.text}>© 2023 Shroom Hats. All rights reserved.</Text>
             </View>
-        </View>
+        </ScrollView>
     );
 };
 
